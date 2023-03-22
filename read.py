bbf392efbd3e3d2f8ad3c619e39ee9bf124ffb51
Vito Genovese <genovese.work@protonmail.com>
2023-03-22 08:55:14 +0100

    before structure

--- read.py
+++ read.py
@@ -15,25 +15,24 @@ per = "records/persona.txt"
 # data = parse_file(per)
 # finish = time.perf_counter()
 
-def tag_mapper(line: str):
-    if line.find("DOCUMENT") >= 0:
-            pass
-    elif line.find("FORM=") >= 0:
-        pass
-    else:
-        try:
-            tag, v = line.split("|", 1)
-            tag = tag[1:4]
-            if result.get(tag):
-                result[tag].append(v)
-            else:
-                result[tag] = [v]
-        except Exception:
-            pass
+# def tag_mapper(line: str):
+#     if line.find("DOCUMENT") >= 0:
+#             pass
+#     elif line.find("FORM=") >= 0:
+#         pass
+#     else:
+#         try:
+#             tag, v = line.split("|", 1)
+#             tag = tag[1:4]
+#             if result.get(tag):
+#                 result[tag].append(v)
+#             else:
+#                 result[tag] = [v]
+#         except Exception:
+#             pass
 def parse_file(file_name: str):
     result = {} 
     file = open(file_name, mode="r", encoding="utf-8", errors="ignore")
-    counter = 0
     bne_id = None
     for line in file:
         if line.find("DOCUMENT") >= 0:
@@ -46,11 +45,12 @@ def parse_file(file_name: str):
                 if is_tag:
                     tag, v = line.split("|", 1)
                 else:
-                    _, v = line.split("|",1)
+                    result[tag].append({bne_id:v})
+                    continue
                 v = f"|{v}"
                 tag = tag[1:4]
                 if tag == "001":
-                    bne_id = v[1:-1]
+                    bne_id = v[2:-1]
                     result[bne_id] = {tag:v}
                 if result.get(bne_id):
                     result[bne_id][tag] = v
@@ -61,13 +61,26 @@ def parse_file(file_name: str):
             except Exception:
                 pass
     file.close()
-    # file = open("results/test.json", "w", encoding="utf-8")
-    # json.dump(result, file, indent=4)
-    # file.close()
+    file = open("results/test.json", "w", encoding="utf-8")
+    json.dump(result, file, indent=4)
+    file.close()
     return result
     
 start = time.perf_counter()
 data = parse_file(per)
+
+
+tag = "375"
+ss = "mas"
+ids = []
+for pair in data.get(tag):
+    k,v = tuple(*pair.items())
+    if v.find(ss) >= 0:
+        ids.append(k)
+result = []
+for v in ids:
+    result.append(data.get(v))
+# print(result)
 finish = time.perf_counter()
 
 print("-".center(50, "-"))
